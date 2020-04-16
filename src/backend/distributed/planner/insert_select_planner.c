a47224bdbc81650ef501bf794af2998df4f15ef0
Philip Dubé <gunkmute@gmail.com>
2020-04-16 11:19:06 +0000

    Use PartitionMethod/TableReplicationModel more often

--- src/backend/distributed/planner/insert_select_planner.c
+++ src/backend/distributed/planner/insert_select_planner.c
@@ -428,9 +428,7 @@ RouterModifyTaskForShardInterval(Query *originalQuery, ShardInterval *shardInter
 
 	uint64 shardId = shardInterval->shardId;
 	Oid distributedTableId = shardInterval->relationId;
-	CitusTableCacheEntryRef *cacheRef = GetCitusTableCacheEntry(distributedTableId);
-	char replicationModel = cacheRef->cacheEntry->replicationModel;
-	ReleaseTableCacheEntry(cacheRef);
+	char replicationModel = TableReplicationModel(distributedTableId);
 
 	PlannerRestrictionContext *copyOfPlannerRestrictionContext = palloc0(
 		sizeof(PlannerRestrictionContext));
